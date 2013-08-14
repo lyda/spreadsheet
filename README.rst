625af82417a8f7762f7d031b41626565f8396886
Kevin Lyda <kevin.lyda@deri.org>
2013-08-14 12:49:17 +0100

    Initial changes to publish this.

---
+++ README.rst
@@ -0,0 +1,92 @@
+===========
+spreadsheet
+===========
+
+Command Line Tool To Manipulate Google Spreadsheets
+===================================================
+The ``spreadsheet`` tool is a command line tool to manipulate Google
+spreadsheets. It specifically works on spreadsheets where row 1 is
+a series of column titles and there is one or more columns that can
+act as a unique key.
+
+When running it it will create three config files in the current
+directory:
+
+* ``.app.json``: Configuration file with the app configuration.  By
+  default these are anonymous, but if you have Google Apps for your
+  domain, you might want to set these. TODO: Explain how and provide
+  urls to Google help docs and the app admin panel for this.
+* ``.auth.json``: These store your auth credentials. The ``spreadsheet``
+  tool will manipulate these.
+* ``.ss.json``: This records the spreadsheet and worksheet you chose
+  as well as any infor the ``spreadsheet`` tool has cached.
+
+If these need info from you, you will be prompted.
+
+Commands
+~~~~~~~~
+
+All references to "column" mean the title of the column.
+
+spreadsheet app_conf "consumer_key" "consumer_secret" "google_apps_domain"
+spreadsheet list "key column"
+spreadsheet update "key column" key "value column" value
+spreadsheet remember headers
+spreadsheet remember "column"
+spreadsheet forget headers
+spreadsheet forget "column"
+
+Dependencies
+~~~~~~~~~~~~
+
+Depends on the following modules: ``gdata``, ``json``, ``gflags``.
+
+Contributions
+=============
+Contributions are welcome!
+
+Unit tests are kind of difficult since I haven't found a good mock
+spreadsheet.
+
+The packaged version is available via ``pip`` or ``easy_install``
+as ``spreadsheet``. The project page is on `pypi`_:
+
+The source code is available in the following locations:
+
+* Bitbucket: https://bitbucket.org/lyda/spreadsheet/
+* code.google: https://code.google.com/p/spreadsheet/
+* Github: https://github.com/lyda/spreadsheet
+* Gitorious: https://gitorious.org/uu/spreadsheet
+* Sourceforge: https://sourceforge.net/p/spreadsheet
+
+Pull requests on any of those platforms or emailed patches are fine.
+Opening issues on github is easiest, but I'll check any of them.
+
+TODO
+====
+* Currently authentication redirects to a local url. Document that
+  url and give the user the option to change it.
+* Likewise, it currently listens on localhost and a dynamically
+  chosen port. Provide a way to supply that.
+* Clean up how the auth token is found.
+* Some unit tests of some sort.
+* A way to add rows.  And a way to delete them.
+* Finish allowing users to specify the title row number.
+* A command similar to update that allows users to specify the
+  row/column directly.
+* Have caching be more seemless. When using cache, do sanity checks
+  to make sure it's correct (check the search column and the header)
+  and then recache if it's a miss.  Keep the forget/remember commands
+  in case the user knows the caches are wrong and can use these to
+  tell us ahead of time.
+* Handle a bunch of possible exceptions in the gdata API.
+* Is `issue580`_ fixed yet? If so remove the ``str()`` calls for
+GetCells parameters.
+
+Credits
+=======
+- `Kevin Lyda`_: Who got burned one too many times by broken crontabs.
+
+.. _`Kevin Lyda`: https://github.com/lyda
+.. _`pypi`: https://pypi.python.org/pypi/spreadsheet
+.. _`issue580`: https://code.google.com/p/gdata-python-client/issues/detail?id=580
---
+++ setup.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+"""Installer for spreadsheet.
+
+This installs the spreadsheet command.
+"""
+
+import os
+import sys
+from distutils import file_util
+from distutils import log
+from distutils.command.install import install
+from distutils.core import setup
+from distutils.core import Command
+if sys.version_info < (2, 7):
+  import unittest2 as unittest
+else:
+  import unittest
+
+BASE_DIR = os.path.dirname(globals().get('__file__', os.getcwd()))
+
+
+class TestCmd(Command):
+  description = 'Runs all available tests.'
+  user_options = []
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    test_dir = os.path.join(BASE_DIR, 'tests')
+
+    tests = unittest.TestLoader().discover(test_dir)
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(tests)
+    if not result.wasSuccessful():
+      sys.exit(1)
+
+
+class CleanCmd(Command):
+  description = 'Remove all generated files.'
+  user_options = []
+
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    # Configure for this project.
+    suffixes2del = ['MANIFEST', '.pyc', '.test_out']
+    dirs2del = ['./build', './dist', './tests/logs', './.tox',
+                './.coverage', './htmlcov', './__pycache__',
+                './tests/__pycache__']
+    dirs2ign = ['./.git']
+    # End config.
+    doomed = set()
+    # Change to base dir.
+    os.chdir(BASE_DIR)
+    for root, dirs, files in os.walk('.'):
+      # Handle root dirs.
+      if root in dirs2ign:
+        continue
+      if root in dirs2del:
+        doomed.add(root)
+      # Handle files.
+      for f in files:
+        accused = os.path.join(root, f)
+        for suffix in suffixes2del:
+          if f.endswith(suffix):
+            doomed.add(accused)
+            break
+        if accused not in doomed:
+          for d2del in dirs2del:
+            if accused.startswith(d2del):
+              doomed.add(accused)
+              break
+      # Handle dirs.
+      for d in dirs:
+        accused = os.path.join(root, d)
+        for d2ign in dirs2ign:
+          if accused.startswith(d2ign):
+            dirs.remove(d)
+            break
+        if d in dirs:
+          for d2del in dirs2del:
+            if accused.startswith(d2del):
+              doomed.add(accused)
+              break
+    # Probably not required, but just to be safe.
+    for accused in doomed:
+      for d2ign in dirs2ign:
+        if accused.startswith(d2ign):
+          doomed.remove(accused)
+          break
+    for accused in sorted(doomed, reverse=True):
+      log.info('removing "%s"', os.path.normpath(accused))
+      if not self.dry_run:
+        try:
+          os.unlink(accused)
+        except:
+          try:
+            os.rmdir(accused)
+          except:
+            log.warn('unable to remove "%s"', os.path.normpath(accused))
+
+
+class InstallCmd(install):
+  user_options = install.user_options[:]
+  user_options.extend([('manprefix=', None,
+                        'installation prefix for man pages')])
+
+  def initialize_options(self):
+    self.manprefix = None
+    install.initialize_options(self)
+
+  def finalize_options(self):
+    install.finalize_options(self)
+    if self.manprefix is None:
+      self.manprefix = os.path.join(self.install_scripts,
+                                    '..', 'share', 'man')
+
+  def run(self):
+    install.run(self)
+    manpages = []
+    if self.manprefix:
+      for manpage in manpages:
+        section = manpage.split('/')[-1].split('.')[-1]
+        manpage_file = manpage.split('/')[-1]
+        manpage_dir = os.path.realpath(os.path.join(self.manprefix,
+                                                    'man%s' % section))
+        if not self.dry_run:
+          try:
+            os.makedirs(manpage_dir)
+          except OSError:
+            pass
+        file_util.copy_file(manpage,
+                            os.path.join(manpage_dir, manpage_file),
+                            dry_run=self.dry_run)
+
+# Only override install if not being run by setuptools.
+cmdclass = {'test': TestCmd,
+            'dist_clean': CleanCmd,
+            }
+if 'setuptools' not in dir():
+  cmdclass['install'] = InstallCmd
+
+setup(
+  cmdclass=cmdclass,
+  name='spreadsheet',
+  version='1.0',
+  url='https://github.com/lyda/spreadsheet',
+  author='Kevin Lyda',
+  author_email='kevin@ie.suberic.net',
+  description='A tool to manipulate Google Spreadsheets',
+  long_description=open('README.rst').read(),
+  py_modules=[],
+  scripts=['spreadsheets', ],
+  keywords='google spreadsheet gdata',
+  license='GNU General Public License v3',
+  platforms=['POSIX'],
+  # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
+  classifiers=['Development Status :: 5 - Production/Stable',
+               'Environment :: Console',
+               'License :: OSI Approved'
+               ' :: GNU General Public License v3 (GPLv3)',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 2.5',
+               'Programming Language :: Python :: 2.6',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Utilities',
+               ],
+)
